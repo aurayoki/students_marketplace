c032fac6222c39ba9c933ff5bd1aba66d92f90b5
Hasan <tsitskievhasan@yandex.ru>
2021-02-05 00:02:19 +0300

    Добавил метод findByPhone, findAll

--- src/main/java/com/jm/marketplace/service/UserService.java
+++ src/main/java/com/jm/marketplace/service/UserService.java
@@ -3,6 +3,7 @@ package com.jm.marketplace.service;
 import com.jm.marketplace.model.User;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 public interface UserService {
@@ -11,7 +12,11 @@ public interface UserService {
 
     List<User> findUserByBirthday(LocalDate date);
 
+    List<User> findAll();
+
     User findByEmail(String email);
 
     boolean checkByEmail(String email);
+
+    User findByPhone(String phone);
 }
--- src/main/java/com/jm/marketplace/service/UserServiceImpl.java
+++ src/main/java/com/jm/marketplace/service/UserServiceImpl.java
@@ -2,14 +2,13 @@ package com.jm.marketplace.service;
 
 import com.jm.marketplace.dao.UserDao;
 import com.jm.marketplace.exception.UserEmailExistsException;
+import com.jm.marketplace.exception.UserPhoneExistsException;
 import com.jm.marketplace.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -33,12 +32,22 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    @Override
     public User findByEmail(String email) {
-        return userDao.findByEmail(email).orElseThrow(() -> new UserEmailExistsException("User have base"));
+        return userDao.findByEmail(email).orElseThrow(() -> new UserEmailExistsException("Пользователь с такой почтой не найден"));
     }
 
     @Override
     public boolean checkByEmail(String email) {
         return userDao.findByEmail(email).isPresent();
+    }
+
+    @Override
+    public User findByPhone(String phone) {
+        return userDao.findByPhone(phone).orElseThrow(() -> new UserPhoneExistsException("Пользователь с таким номером телефона не найден"));
     }
 }
