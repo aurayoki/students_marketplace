3f3c0c889a1583420146483b170cf1ea05db312d
Hasan <tsitskievhasan@yandex.ru>
2021-02-12 00:17:40 +0300

    Добавлена регистрация

--- src/main/java/com/jm/marketplace/service/city/CityService.java
+++ src/main/java/com/jm/marketplace/service/city/CityService.java
@@ -1,10 +1,13 @@
 package com.jm.marketplace.service.city;
 
+import com.jm.marketplace.dto.CityDto;
 import com.jm.marketplace.model.City;
 
 import java.util.List;
 
 public interface CityService {
 
-    List<City> getAllCity();
+    List<CityDto> getAllCity();
+
+    CityDto findById(Long id);
 }
--- src/main/java/com/jm/marketplace/service/city/CityServiceImpl.java
+++ src/main/java/com/jm/marketplace/service/city/CityServiceImpl.java
@@ -1,6 +1,9 @@
 package com.jm.marketplace.service.city;
 
+import com.jm.marketplace.config.mapper.MapperFacade;
 import com.jm.marketplace.dao.CityDao;
+import com.jm.marketplace.dto.CityDto;
+import com.jm.marketplace.exception.CityNotFoundException;
 import com.jm.marketplace.model.City;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,14 +14,23 @@ import java.util.List;
 public class CityServiceImpl implements CityService{
 
     private final CityDao cityDao;
+    private final MapperFacade mapperFacade;
 
     @Autowired
-    public CityServiceImpl(CityDao cityDao) {
+    public CityServiceImpl(CityDao cityDao, MapperFacade mapperFacade) {
         this.cityDao = cityDao;
+        this.mapperFacade = mapperFacade;
     }
 
     @Override
-    public List<City> getAllCity() {
-        return cityDao.findAll();
+    public List<CityDto> getAllCity() {
+        return mapperFacade.mapAsList(cityDao.findAll(), CityDto.class);
+    }
+
+    @Override
+    public CityDto findById(Long id) {
+        City city = cityDao.findById(id).orElseThrow(() ->
+                new CityNotFoundException(String.format("City not found by id: %s", id)));
+        return mapperFacade.map(city, CityDto.class);
     }
 }
