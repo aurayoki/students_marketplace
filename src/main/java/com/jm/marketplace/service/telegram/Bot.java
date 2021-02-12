26a13f44b5b0a368658d84725ec11dbc56efd191
mark-laptop <ndg23@bk.ru>
2021-02-12 21:49:38 +0300

    Исправления

---
+++ src/main/java/com/jm/marketplace/service/telegram/Bot.java
@@ -0,0 +1,106 @@
+package com.jm.marketplace.service.telegram;
+
+import com.jm.marketplace.dto.goods.AdvertisementDto;
+import com.jm.marketplace.service.advertisement.AdvertisementService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Slf4j
+@PropertySource(value = "classpath:telegram.properties", encoding = "UTF-8")
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${bot.button.list_advertisement}")
+    private String listAdvertisement;
+
+    @Value("${bot.name}")
+    private String botUsername;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    private final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+    private final List<KeyboardRow> keyboard = new ArrayList<>();
+
+    private AdvertisementService advertisementService;
+
+    @Autowired
+    public void setAdvertisementService(AdvertisementService advertisementService) {
+        this.advertisementService = advertisementService;
+    }
+
+    @PostConstruct
+    public void init() {
+        configKeyboard();
+    }
+
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChatId().toString());
+
+            setButtons(sendMessage);
+
+            setMessage(sendMessage);
+
+            execute(sendMessage);
+            log.info("Send telegram message, username: {}", update.getMessage().getFrom().getUserName());
+        } catch (Exception e) {
+            log.warn("Error with send telegram message");
+            e.printStackTrace();
+        }
+    }
+
+    private void setMessage(SendMessage sendMessage) {
+
+        List<AdvertisementDto> advertisementDtos = advertisementService.findAll();
+        StringBuilder sb = new StringBuilder(advertisementDtos.size());
+        int count = 1;
+        for (AdvertisementDto advertisementDto : advertisementDtos) {
+            sb.append(count).append("\n");
+            sb.append(advertisementDto.getName()).append("\n");
+            sb.append(advertisementDto.getPrice()).append("\n");
+            sb.append(advertisementDto.getDescription()).append("\n");
+            sb.append("-------------------");
+            sb.append("\n");
+            count++;
+        }
+        sendMessage.setText(sb.toString());
+    }
+
+    private void setButtons(SendMessage sendMessage) {
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(listAdvertisement);
+        keyboard.clear();
+        keyboard.add(keyboardFirstRow);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+    }
+
+    private void configKeyboard() {
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+}
